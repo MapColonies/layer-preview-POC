40e272886a1a363874db8ff0d977e68f005eb359
assafchamoy <assafchamoy97@gmail.com>
2022-06-06 17:37:24 +0300

    chore: 3d tileset mock data

--- cesiumClientWeb/script.js
+++ cesiumClientWeb/script.js
@@ -5,7 +5,8 @@ const tokenHeader = { 'X-API-KEY': RASTER_TOKEN };
 const viewer = new Cesium.Viewer('cesiumContainer', { baseLayerPicker: false });
 
 // Remove stock cesium's base layer
-viewer.imageryLayers.remove(viewer.imageryLayers.get(0));
+// viewer.imageryLayers.remove(viewer.imageryLayers.get(0));
+viewer.scene.globe.baseColor = Cesium.Color.WHITE
 
 viewer.animation.container.style.visibility = 'hidden';
 viewer.timeline.container.style.visibility = 'hidden';
@@ -13,32 +14,28 @@ viewer.forceResize();
 
 // const provider = new Cesium.WebMapTileServiceImageryProvider({
 //     url: new Cesium.Resource({
+      
 //       url: `LAYER URL`,
 //       /* Don't forget to include the authentication header */
-//       headers: tokenHeader,
+//       // headers: tokenHeader,
 //     }),
 //   })
 
 //   const layer = viewer.imageryLayers.addImageryProvider(provider);
 
-//   viewer.flyTo(layer);
-
 
 const tileset = viewer.scene.primitives.add(
     new Cesium.Cesium3DTileset({
       url:new Cesium.Resource({
-        url: '3d_TILESET',     
-        headers: tokenHeader
+        url: '/mock/tileset_1/tileset.json',     
+        // headers: tokenHeader
       })
     })
   );
  
   viewer.flyTo(tileset, { duration: 0 })
   .then(()=> {
-    viewer.camera.position = new Cesium.Cartesian3(1339662.456792876, -4649056.358393923, 4143333.324980662);
-    viewer.camera.direction = new Cesium.Cartesian3(-0.8640802171411865, -0.407597507677319, -0.29534666085885897);
-    viewer.camera.up = new Cesium.Cartesian3(0.14337653309767578, -0.7617546056359903, 0.6318014645038804);
-  
+    viewer.camera.zoomIn(80)
   }).finally(()=>{
     const img_3d = document.createElement('img');
     img_3d.src = './images/3d_icon.jpg';
