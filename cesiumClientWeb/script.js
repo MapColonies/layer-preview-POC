2b74937c4d2570d301a75e3a6ed935fb1d1fa2db
Assaf Chamoy <assafchamoy97@gmail.com>
2022-06-07 13:37:25 +0300

    feat: add mc icons and query params

--- cesiumClientWeb/script.js
+++ cesiumClientWeb/script.js
@@ -1,47 +1,107 @@
 const RASTER_TOKEN = '';
 const tokenHeader = { 'X-API-KEY': RASTER_TOKEN };
+const URL_PARAM = 'url';
+const PRODUCT_TYPE_PARAM = 'type';
+const PRODUCT_TYPE_RASTER = 'raster';
+const PRODUCT_TYPE_3D = '3d';
+
+// Helpers
+
+const getParameterByName = name => {
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop)
+  });
+
+  return params[name];
+};
+
+const appendIconByProductType = productType => {
+  let iconClassName;
+
+  switch (productType) {
+    case PRODUCT_TYPE_3D:
+      iconClassName = 'mc-icon-Map-3D';
+      break;
+    case PRODUCT_TYPE_RASTER:
+      iconClassName = 'mc-icon-Map-Raster';
+      break;
+    default:
+      iconClassName = 'mc-icon-Map-Raster';
+      break;
+  }
+
+  const iconSpan = document.createElement('span');
+  iconSpan.classList.add(iconClassName);
+  iconSpan.id = 'layerIcon';
+
+  document.querySelector('body').appendChild(iconSpan);
+};
+
+const render3DTileset = () => {
+  const tileset = viewer.scene.primitives.add(
+        new Cesium.Cesium3DTileset({
+          url: new Cesium.Resource({
+            url,
+            headers: tokenHeader
+          })
+        })
+    );
+
+  return viewer.flyTo(tileset, { duration: 0 });
+};
+
+const renderRasterLayer = () => {
+  const provider = new Cesium.WebMapTileServiceImageryProvider({
+    url: new Cesium.Resource({
+      url,
+      headers: tokenHeader
+    })
+  });
+
+  const layer = viewer.imageryLayers.addImageryProvider(provider);
+
+  return viewer.flyTo(layer, { duration: 0 });
+};
+
+// --------
 
 // Setup Cesium viewer first.
 const viewer = new Cesium.Viewer('cesiumContainer', { baseLayerPicker: false });
 
 // Remove stock cesium's base layer
-// viewer.imageryLayers.remove(viewer.imageryLayers.get(0));
-viewer.scene.globe.baseColor = Cesium.Color.WHITE
+viewer.imageryLayers.remove(viewer.imageryLayers.get(0));
+viewer.scene.globe.baseColor = Cesium.Color.WHITE;
 
 viewer.animation.container.style.visibility = 'hidden';
 viewer.timeline.container.style.visibility = 'hidden';
 viewer.forceResize();
 
-// const provider = new Cesium.WebMapTileServiceImageryProvider({
-//     url: new Cesium.Resource({
-      
-//       url: `LAYER URL`,
-//       /* Don't forget to include the authentication header */
-//       // headers: tokenHeader,
-//     }),
-//   })
+const url = getParameterByName(URL_PARAM);
+const productType = getParameterByName(PRODUCT_TYPE_PARAM);
 
-//   const layer = viewer.imageryLayers.addImageryProvider(provider);
+switch (productType) {
+  case PRODUCT_TYPE_3D: {
+    render3DTileset()
+            .then(() => {
+              viewer.camera.zoomIn(50);
+            })
+            .finally(() => {
+              appendIconByProductType(PRODUCT_TYPE_3D);
+            });
 
+    break;
+  }
+  case PRODUCT_TYPE_RASTER: {
+    renderRasterLayer()
+            .then(() => {
+              viewer.camera.zoomIn(50);
+            })
+            .finally(() => {
+              appendIconByProductType(PRODUCT_TYPE_RASTER);
+            });
+    break;
+  }
 
-const tileset = viewer.scene.primitives.add(
-    new Cesium.Cesium3DTileset({
-      url:new Cesium.Resource({
-        url: '/mock/tileset_1/tileset.json',     
-        // headers: tokenHeader
-      })
-    })
-  );
- 
-  viewer.flyTo(tileset, { duration: 0 })
-  .then(()=> {
-    viewer.camera.zoomIn(80)
-  }).finally(()=>{
-    const img_3d = document.createElement('img');
-    img_3d.src = './images/3d_icon.jpg';
-    img_3d.id = 'layerIcon';
-
-    document.querySelector('body').appendChild(img_3d);
-  })
-
-
+  default:
+    break;
+}
--- puppeteer/index.js
+++ puppeteer/index.js
@@ -1,3 +1,4 @@
+const fs = require("fs/promises");
 const puppeteer = require("puppeteer");
 
 (async function() {
@@ -5,13 +6,14 @@ const puppeteer = require("puppeteer");
     const page = await browser.newPage();
     try{
         await page.setViewport({ width: 1200, height: 600});
-        await page.goto('http://localhost:4000');
+        await page.goto('http://localhost:4000/?url=/mock/tileset_1/tileset.json&type=3d');
         await page.waitForSelector('#layerIcon');
         const cesiumElem = await page.$('#cesiumContainer');
+        await fs.mkdir('./puppeteer/screenshots', { recursive: true });
         await cesiumElem.screenshot({ path: './puppeteer/screenshots/model.png' });
         await browser.close();
     }catch(e) {
-        console.log(e);
+        console.error(e);
         await browser.close();
     }
 })()
