6cd74427b1c3e83789121abc40e11220a3763901
Assaf Chamoy <assafchamoy97@gmail.com>
2022-06-07 18:18:58 +0300

    feat: add turf, try to figure out zoom level and center of the bbox

--- cesiumClientWeb/script.js
+++ cesiumClientWeb/script.js
@@ -4,6 +4,20 @@ const URL_PARAM = 'url';
 const PRODUCT_TYPE_PARAM = 'type';
 const PRODUCT_TYPE_RASTER = 'raster';
 const PRODUCT_TYPE_3D = '3d';
+const MAX_APPROPRIATE_ZOOM_KM = 2;
+
+
+// Setup Cesium viewer first.
+const viewer = new Cesium.Viewer('cesiumContainer', { baseLayerPicker: false });
+const ellipsoid = viewer.scene.mapProjection.ellipsoid;
+
+// Remove stock cesium's base layer
+viewer.imageryLayers.remove(viewer.imageryLayers.get(0));
+viewer.scene.globe.baseColor = Cesium.Color.WHITE;
+
+viewer.animation.container.style.visibility = 'hidden';
+viewer.timeline.container.style.visibility = 'hidden';
+viewer.forceResize();
 
 // Helpers
 
@@ -37,6 +51,45 @@ const appendIconByProductType = productType => {
   document.querySelector('body').appendChild(iconSpan);
 };
 
+const getExtentRect = () => {
+  const rect = new Cesium.Rectangle();
+  const computedExtentRect = viewer.camera.computeViewRectangle(viewer.scene.globe.ellipsoid, rect);
+
+  return computedExtentRect;
+}
+
+const getExtentSize = () => {
+  const extent = getExtentRect(); 
+  const NE = Cesium.Cartographic.toCartesian(Cesium.Rectangle.northeast(extent));
+  const SW = Cesium.Cartographic.toCartesian(Cesium.Rectangle.southwest(extent));
+
+  return Cesium.Cartesian3.distance(NE, SW) / 1000 // To get KM distance;
+
+}
+
+const getCameraHeight = () => {
+  const height = ellipsoid.cartesianToCartographic(viewer.camera.position).height;
+  
+  return Math.floor(height * 0.001); // KM
+}
+
+const setCameraToProperHeightAndPos = () => {
+  const cameraPosCartographic = ellipsoid.cartesianToCartographic(viewer.camera.position);
+
+  if(getCameraHeight() > MAX_APPROPRIATE_ZOOM_KM) {
+    cameraPosCartographic.height = MAX_APPROPRIATE_ZOOM_KM * 1000; // Convert to meters
+  }
+
+  viewer.camera.position = ellipsoid.cartographicToCartesian(cameraPosCartographic);
+}
+
+// --------
+
+const url = getParameterByName(URL_PARAM);
+const productType = getParameterByName(PRODUCT_TYPE_PARAM);
+
+console.log(url, productType);
+
 const render3DTileset = () => {
   const tileset = viewer.scene.primitives.add(
         new Cesium.Cesium3DTileset({
@@ -44,6 +97,7 @@ const render3DTileset = () => {
             url,
             headers: tokenHeader
           })
+          // url: Cesium.IonResource.fromAssetId(75343)
         })
     );
 
@@ -63,27 +117,16 @@ const renderRasterLayer = () => {
   return viewer.flyTo(layer, { duration: 0 });
 };
 
-// --------
 
-// Setup Cesium viewer first.
-const viewer = new Cesium.Viewer('cesiumContainer', { baseLayerPicker: false });
 
-// Remove stock cesium's base layer
-viewer.imageryLayers.remove(viewer.imageryLayers.get(0));
-viewer.scene.globe.baseColor = Cesium.Color.WHITE;
 
-viewer.animation.container.style.visibility = 'hidden';
-viewer.timeline.container.style.visibility = 'hidden';
-viewer.forceResize();
-
-const url = getParameterByName(URL_PARAM);
-const productType = getParameterByName(PRODUCT_TYPE_PARAM);
+// Render products
 
 switch (productType) {
   case PRODUCT_TYPE_3D: {
     render3DTileset()
             .then(() => {
-              viewer.camera.zoomIn(50);
+              setCameraToProperHeightAndPos()
             })
             .finally(() => {
               appendIconByProductType(PRODUCT_TYPE_3D);
@@ -94,7 +137,7 @@ switch (productType) {
   case PRODUCT_TYPE_RASTER: {
     renderRasterLayer()
             .then(() => {
-              viewer.camera.zoomIn(50);
+              // viewer.camera.zoomIn(50);
             })
             .finally(() => {
               appendIconByProductType(PRODUCT_TYPE_RASTER);
@@ -105,3 +148,5 @@ switch (productType) {
   default:
     break;
 }
+
+// --------
